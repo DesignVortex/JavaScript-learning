dfbf7d2c587f8e218e3ce518bb62a7ab3cd13726
DesignVortex <146312470+DesignVortex@users.noreply.github.com>
2023-09-28 09:49:56 +0000

    datatypes discussion

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";   // treat as newer version of javascript
+
+let name = "Vishal";
+let age = 37;
+let isLoggedIn = true;
+
+// number  => 2 - power of 53
+// bigint
+// string = "vishal"
+// boollean => true or flase
+// null => standalone value
+// undefined => 
+// symbol
+
+// object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 123456
+let emailID = "vishal@gmail.com"
+var password = "234567"
+userCity = "Vadodara"
+let accountState;
+
+// accountID = 234.  // not allowed
+
+emailID = "vg@vg.com";
+password = "321";
+userCity = "Gandhinagar";
+
+console.table([accountID, emailID, password, userCity, accountState]);
+
+
+console.log(accountID);
