868f2c5de14504eaa0e7f271bfa0fd162ff92a7d
DesignVortex <146312470+DesignVortex@users.noreply.github.com>
2023-10-01 05:46:47 +0000

    number method

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,18 @@
+const score = 400;
+
+const balance = new Number(4000)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+const otherNumber = 123.36568;
+// console.log(otherNumber.toPrecision(4));
+
+const millions = 10000000000
+
+console.log(millions.toLocaleString());
+
+
