77b6a6cee8b8ad9b2abd7200afa1802d9931cc16
DesignVortex <146312470+DesignVortex@users.noreply.github.com>
2023-10-01 07:28:23 +0000

    date and time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,25 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// let myNewDate = new Date(2023, 3, 21);
+// let myNewDate = new Date(2023, 3, 21 , 5 , 7);
+let myNewDate = new Date("2300-08-24");
+
+// console.log(myNewDate.toLocaleString());
+
+// Time ----------------------
+
+let myTimeStamp = Math.round(Date.now()/1000);
+
+// console.log(myTimeStamp);
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+
+
