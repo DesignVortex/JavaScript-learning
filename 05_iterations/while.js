2aa7acc06d22747ccc297787fee480f85a6eca84
DesignVortex <146312470+DesignVortex@users.noreply.github.com>
2023-10-07 13:12:13 +0000

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,18 @@
+// while loop
+
+let i = 0;
+
+while (i <= 10) {
+    // console.log(`Value of i is : ${i}`);
+    i = i + 3;
+}
+
+const myArray = ["Flash", "Batman", "Superman"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+    
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
